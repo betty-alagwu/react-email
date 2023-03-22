d1c2dcae6b384fec1d4194c8626cdc4806d0e961
Betty Alagwu <bettyalagwu@gmail.com>
2023-03-22 16:58:53 +0100

    chore: setup prepare and symlink script to demo

--- demo/scripts/prepare.js
+++ demo/scripts/prepare.js
@@ -1,15 +1,15 @@
-const Path = require('path')
-const Symlink = require('symlink-dir')
+const Path = require('path');
+const Symlink = require('symlink-dir');
 
-const client = Path.resolve(process.cwd(), '..', 'client')
-const REACT_EMAIL_ROOT = Path.resolve(process.cwd(), '.react-email')
+const client = Path.resolve(process.cwd(), '..', 'client');
+const REACT_EMAIL_ROOT = Path.resolve(process.cwd(), '.react-email');
 
-async function link() {
- await Symlink(client, REACT_EMAIL_ROOT, {overwrite: true})
+async function linkClientFolderToReactEmailRoot() {
+  await Symlink(client, REACT_EMAIL_ROOT, { overwrite: true });
 
- console.log(`Symlink created from ${client} to ${REACT_EMAIL_ROOT}`)
+  console.log(`Symlink created from ${client} to ${REACT_EMAIL_ROOT}`);
 
- process.exit(0)
+  process.exit(0);
 }
 
-link()
+linkClientFolderToReactEmailRoot();
